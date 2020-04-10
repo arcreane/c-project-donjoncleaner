891555fef88bc18cd4104a86ee20609c42f93531
Ngauv <dxviezas@gmail.com>
2020-04-10 15:56:51 +0200

    Seletion de difficulté

--- ProjetC/Game.cs
+++ ProjetC/Game.cs
@@ -42,29 +42,60 @@ namespace ProjetC
 
         public void GameDifficulty()
         {
+            bool DifficultySelection = true;
+            bool Selection = true;
             Ennemi ennemi = new Ennemi();
-
-            Console.WriteLine("Choisissez la difficulté générale du jeu :\n[1]-Easy\n[2]-MODERATE\n[3]-HARD\n[4]-DEATH");
-            string Answer = Console.ReadLine();
-            switch (Answer)
+            do
             {
-                case "1":
-                    ennemi.Level = Levels.EASY;
-                    break;
-                case "2":
-                    ennemi.Level = Levels.MODERATE;
-                    break;
-                case "3":
-                    ennemi.Level = Levels.HARD;
-                    break;
-                case "4":
-                    ennemi.Level = Levels.DEATH;
-                    break;
-            }
+                do
+                {
+                    Console.WriteLine("\nChoisissez la difficulté générale du jeu :\n[1]-Easy\n[2]-MODERATE\n[3]-HARD\n[4]-DEATH");
+                    string Answer = Console.ReadLine();
+                    switch (Answer)
+                    {
+                        case "1":
+                            ennemi.Level = Levels.EASY;
+                            Selection = true;
+                            break;
+                        case "2":
+                            ennemi.Level = Levels.MODERATE;
+                            Selection = true;
+                            break;
+                        case "3":
+                            ennemi.Level = Levels.HARD;
+                            Selection = true;
+                            break;
+                        case "4":
+                            ennemi.Level = Levels.DEATH;
+                            Selection = true;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("La valeur {0} n'est pas valide !", Answer);
+                            Selection = false;
+                            break;
+                    }
+                } while (!Selection);
+
+                Console.WriteLine("\nVotre mode de jeu a été réglé en : {0}", ennemi.Level);
+                Console.WriteLine("Voulez-vous la modifier ? (yes/no)");
+                string Validation = Console.ReadLine().ToLower();
+                if(Validation == "yes")
+                {
+                    DifficultySelection = true;
+                    Console.Clear();
+                }
+                else
+                {
+                    DifficultySelection = false;
+                }
+            } while (DifficultySelection);
         }
-        /*public void Fight()
+        public void Tuto()
         {
-            ICI LE FIGHT
-        }*/
+            Ennemi ennemi = new Ennemi();
+            Console.WriteLine("{0} :\nHealth : {1}\nAttack : {2}\nDefence : {3}", Entity.Name, Entity.Health, Entity.Attack, Entity.Defence);
+            Console.ReadLine();
+        }
     }
 }
--- ProjetC/Program.cs
+++ ProjetC/Program.cs
@@ -18,12 +18,8 @@ namespace ProjetC
 
             game.GameDifficulty();
 
-            Ennemi ennemi = new Ennemi();
-            Hero hero = new Hero();
+            game.Tuto();
 
-            Console.WriteLine("{0} :\n");
-
-            Console.WriteLine("Ennemi :\n{0}\nHealth : {1}\nAttack : {2}\nDefence : {3}",Entity.Name, Entity.Health, Entity.Attack, Entity.Defence) ;
             Console.ReadLine();
         }
     }
